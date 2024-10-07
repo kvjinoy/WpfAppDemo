91a35cad3901574679f52297bcbd59c790383476
Jinoy Varghese <kvjinoy>
2024-10-07 08:43:56 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfAppDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            labelDemo.Content = textBox.Text;
+        }
+    }
+}
